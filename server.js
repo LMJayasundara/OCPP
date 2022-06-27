4ad5ccae29cc3765d9f3925db3862e08572010ec
LMJayasundara <47645312+LMJayasundara@users.noreply.github.com>
2022-06-27 16:10:32 +0530

    1.5.2
    Check certificate revocation

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const PORT = 8080;
 var passwd = 'pa$$word' // should be get form db
 const clients = new Set();
 
-const server = new https.createServer({
+// config the https options
+const options = {
     cert: fs.readFileSync(`${__dirname}/key/server-crt.pem`),
     key: fs.readFileSync(`${__dirname}/key/server-key.pem`),
     ca: [
@@ -18,12 +19,18 @@ const server = new https.createServer({
     ciphers: 'AES128-GCM-SHA256:AES256-GCM-SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384',
     ecdhCurve: 'secp521r1:secp384r1',
     honorCipherOrder: true
-});
+}
 
+// create the server
+const server = new https.createServer(options);
+
+// create the websocket
 const wss = new WebSocketServer({
     server,
     verifyClient: function (info, cb) {
+        // certificactes auth
         var success = !!info.req.client.authorized;
+        // basic auth
         if(success){
             var authentication = Buffer.from(info.req.headers.authorization.replace(/Basic/, '').trim(),'base64').toString('utf-8');
             if (!authentication)
@@ -48,30 +55,69 @@ const wss = new WebSocketServer({
     rejectUnauthorized: false
 });
 
+// return validate days
+const getDaysBetween = (validFrom, validTo) => {
+    return Math.round(Math.abs(+validFrom - +validTo) / 8.64e7);
+};
+
+const getDaysRemaining = (validFrom, validTo) => {
+    const daysRemaining = getDaysBetween(validFrom, validTo);
+    if (new Date(validTo).getTime() < new Date().getTime()) {
+        return -daysRemaining;
+    }
+    return daysRemaining;
+};
+
+const checkCertificateValidity = (daysRemaining, valid) => {
+    let isValid = true;
+    try {
+        if(daysRemaining <= 0 || !valid) {
+            isValid = false;
+        }
+    } catch(err)  {
+        isValid = false;
+    }
+    return isValid;
+};
+
 wss.on('connection', function (ws, request) {
+    // check revoke status of the certificates
+    const crt = request.connection.getPeerCertificate();
+    const vFrom = crt.valid_from;
+    const vTo = crt.valid_to;
+    var validTo = new Date(vTo);
+
+    var daysRemaining = getDaysRemaining(new Date(), validTo);
+    var valid = request.socket.authorized || false;
+
+    console.log("daysRemaining: ", daysRemaining);
+    console.log("valid: ", valid);
+
+    // add client to the list
     clients.add(request.identity);
     ws.id = request.identity;
     console.log("Connected Charger ID: "  + ws.id);
 
-    ws.on('message', function (msg) {
-        // Broadcast message to all connected clients
-        wss.clients.forEach(function (client) {
-            if(client.id == request.identity){
-                console.log("From client",ws.id,": ", msg.toString());
-
-                let traResRow = fs.readFileSync('./json/TransactionEventResponse.json');
-                let traRes = JSON.parse(traResRow);
-                client.send(JSON.stringify(traRes));
-            };
+    if(checkCertificateValidity(daysRemaining, valid) == true) {
+        ws.on('message', function (msg) {
+            // Broadcast message to all connected clients
+            wss.clients.forEach(function (client) {
+                if(client.id == request.identity){
+                    console.log("From client",ws.id,": ", msg.toString());
+    
+                    let traResRow = fs.readFileSync('./json/TransactionEventResponse.json');
+                    let traRes = JSON.parse(traResRow);
+                    client.send(JSON.stringify(traRes));
+                };
+            });
         });
-    });
-
-    ws.on('close', function () {
-        clients.delete(ws.id);
-        console.log('Client disconnected '+ ws.id);
-        console.log(clients);
-    });
-
+    
+        ws.on('close', function () {
+            clients.delete(ws.id);
+            console.log('Client disconnected '+ ws.id);
+            console.log(clients);
+        });
+    }
 });
 
 server.listen(PORT, ()=>{
